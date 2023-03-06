e1bbf51dfe13f8c849d29d79053fda0e07bdad7b
matus.benko <matus.benko@gmail.com>
2023-03-06 23:36:42 +0100

    Initialize the flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "My own Neovim flake";
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs";
+    };
+    neovim = {
+      url = "github:neovim/neovim/stable?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, neovim }: {
+      packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+      };
+    };
+}
