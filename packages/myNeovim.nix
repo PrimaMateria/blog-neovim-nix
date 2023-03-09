482bc63442e6d0dde266ad96f53be72bafa82390
matus.benko <matus.benko@gmail.com>
2023-03-09 22:16:54 +0100

    Add plugin from Nixpkgs

--- packages/myNeovim.nix
+++ packages/myNeovim.nix
@@ -1,8 +1,10 @@
 { pkgs }:
 let
   customRC = import ../config { inherit pkgs; };
+  plugins = import ../plugins.nix { inherit pkgs; };
 in pkgs.wrapNeovim pkgs.neovim {
       configure = {
         inherit customRC;
+        packages.all.start = plugins;
       };
     }
