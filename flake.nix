d0e6c62cfadc0a12a233a765c784fb51799db17a
matus.benko <matus.benko@gmail.com>
2023-03-09 21:55:39 +0100

    Custom Neovim package

--- flake.nix
+++ flake.nix
@@ -9,11 +9,28 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, neovim }: {
-      packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
+  outputs = { self, nixpkgs, neovim }:
+    let
+      overlayFlakeInputs = prev: final: {
+        neovim = neovim.packages.x86_64-linux.neovim;
+      };
+
+      overlayMyNeovim = prev: final: {
+        myNeovim = import ./packages/myNeovim.nix {
+          pkgs = final;
+        };
+      };
+
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ overlayFlakeInputs overlayMyNeovim ];
+      };
+
+    in {
+      packages.x86_64-linux.default = pkgs.myNeovim;
       apps.x86_64-linux.default = {
         type = "app";
-        program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+        program = "${pkgs.myNeovim}/bin/nvim";
       };
     };
 }
