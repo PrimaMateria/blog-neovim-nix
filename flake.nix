77ac6d78f898acb26097e5cb85b9b5470a3a6e7b
matus.benko <matus.benko@gmail.com>
2023-06-03 17:39:20 +0200

    Support other systems with flake-utils

--- flake.nix
+++ flake.nix
@@ -12,36 +12,45 @@
       url = "github:smartpde/telescope-recent-files";
       flake = false;
     };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
-  outputs = { self, nixpkgs, neovim, telescope-recent-files-src }:
-    let
-      overlayFlakeInputs = prev: final: {
-        neovim = neovim.packages.x86_64-linux.neovim;
+  outputs = { self, nixpkgs, neovim, telescope-recent-files-src, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlayFlakeInputs = prev: final: {
+          neovim = neovim.packages.${prev.system}.neovim;
 
-        vimPlugins = final.vimPlugins // {
-          telescope-recent-files = import ./packages/vimPlugins/telescopeRecentFiles.nix {
-            src = telescope-recent-files-src;
+          vimPlugins = final.vimPlugins // {
+            telescope-recent-files = import ./packages/vimPlugins/telescopeRecentFiles.nix {
+              src = telescope-recent-files-src;
+              pkgs = prev;
+            };
+          };
+        };
+
+        overlayMyNeovim = prev: final: {
+          myNeovim = import ./packages/myNeovim.nix {
             pkgs = prev;
           };
         };
-      };
 
-      overlayMyNeovim = prev: final: {
-        myNeovim = import ./packages/myNeovim.nix {
-          pkgs = final;
+        pkgs = import nixpkgs {
+          system = system;
+          overlays = [ overlayFlakeInputs overlayMyNeovim ];
         };
-      };
 
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        overlays = [ overlayFlakeInputs overlayMyNeovim ];
-      };
+      in
+      {
+        packages = rec {
+          nvim = pkgs.myNeovim;
+          default = nvim;
+        };
 
-    in {
-      packages.x86_64-linux.default = pkgs.myNeovim;
-      apps.x86_64-linux.default = {
-        type = "app";
-        program = "${pkgs.myNeovim}/bin/nvim";
-      };
-    };
+        apps = rec {
+          nvim = flake-utils.lib.mkApp { drv = self.packages.${system}.nvim; };
+          default = nvim;
+        };
+      });
 }
